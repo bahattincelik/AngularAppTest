b4ce4153f0dd80d70e2a71ce826819ee0dd46d89
Bahattin Celik <celik@wave-solutions.de>
2024-09-25 16:43:24 +0200

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,16 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  //templateUrl: './app.component.html',
+  template: `<p>hallo</p>
+  
+  <hr>
+  <div style="background-color:aqua;">
+
+<app-home></app-home>
+
+  </div>
+  `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
     AppRoutingModule
   ],
   providers: [],
